04e8b4aea69fd36c57e6026c415be10f4b0936eb
Duong <duong.nguyen@dirox.net>
2025-03-21 21:50:29 +0700

    added register and dashboard page

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // Đường dẫn đến các tệp của bạn
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
