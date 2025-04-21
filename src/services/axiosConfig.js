82cff20cc0a08a7e262e24c74b47909ff91cb588
Duong <duong.nguyen@dirox.net>
2025-04-21 14:05:43 +0700

    update config env

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5087", // URL của backend API
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5087", // URL của backend API
   headers: {
     "Content-Type": "application/json",
   },
